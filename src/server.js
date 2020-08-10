e03af6c4a993e2401615f80c2b7c604b890410cb
rubens-lavor <haryson07@gmail.com>
2020-08-10 14:04:38 -0300

    template engine

--- src/server.js
+++ src/server.js
@@ -1,15 +1,59 @@
-const express = require('express')
-const server = express()
+const proffys = [
+    {
+        name:"Diego Fernandes",
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp:"9-8323-3433",
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost: "20",
+        weekday:[0],
+        time_from:[720],
+        time_to:[1220],
+    },
+    {
+        name:"Daniele Evangelista",
+        avatar:"https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp:"9-8323-3433",
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject:"Química",
+        cost: "20",
+        weekday:[1],
+        time_from:[720],
+        time_to:[1220],
+    }
+]
 
-server.use(express.static("public"))
-.get("/", (requisicao, resposta) => {
-    return resposta.sendFile(__dirname + "/views/index.html");
+
+function pageLanding(req, resposta) {
+    return resposta.render("index.html");
+}
+
+function pageStudy(req, resposta) {
+    return resposta.render("study.html", { proffys });
+}
+
+
+function pageGiveClasses(req, resposta) {
+    return resposta.render("give-classes.html");
+}
+
+const express = require('express');
+const server = express();
+
+
+//configurar nunjucks
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
 })
-.get("/study", (requisicao, resposta) => {
-    return resposta.sendFile(__dirname + "/views/study.html");
-})
-.get("/give-classes", (requisicao, resposta) => {
-    return resposta.sendFile(__dirname + "/views/give-classes.html");
-})
+
+server
+//configurar arquivos estáticos(css, scrits, imgaens)
+.use(express.static("public"))/**busca da pasta public */
+// rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
 .listen(8080)
 
