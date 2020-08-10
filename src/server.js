f2a706b164da15bb3549580af41f9a3a2f916ddb
rubens-lavor <haryson07@gmail.com>
2020-08-10 11:40:20 -0300

    rotas

--- src/server.js
+++ src/server.js
@@ -6,7 +6,10 @@ server.use(express.static("public"))
     return resposta.sendFile(__dirname + "/views/index.html");
 })
 .get("/study", (requisicao, resposta) => {
-    return resposta.send("study");
+    return resposta.sendFile(__dirname + "/views/study.html");
+})
+.get("/give-classes", (requisicao, resposta) => {
+    return resposta.sendFile(__dirname + "/views/give-classes.html");
 })
 .listen(8080)
 
